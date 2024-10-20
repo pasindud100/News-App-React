8e9e79251ff72dd6f8af990b320c619a25ed68aa
pasindu100 <dilshanp5400@gmail.com>
2024-10-20 22:32:13 +0530

    create responsive navbar

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+<<<<<<< HEAD
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';//
+=======
+>>>>>>> 895c57a (create responsive navbar)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
