dc6ca7fb1b0af1ae02487c5f80f9b31c58c6db42
pasindu100 <dilshanp5400@gmail.com>
2024-10-23 18:15:33 +0530

    configure packages

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';//
 =======
 >>>>>>> 895c57a (create responsive navbar)
+=======
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+>>>>>>> 88ecb36 (configure packages)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
