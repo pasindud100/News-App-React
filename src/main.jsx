f0feb1db653429f936f93ebf131fabfdd6010d75
pasindu100 <dilshanp5400@gmail.com>
2024-10-23 20:41:29 +0530

    Your commit message

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';//
-=======
->>>>>>> 895c57a (create responsive navbar)
-=======
+
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
->>>>>>> 88ecb36 (configure packages)
+
+
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';//
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
